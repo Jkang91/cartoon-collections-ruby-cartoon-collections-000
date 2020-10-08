2a77b10cbc0adf4cfbaf78dc1e7f29c2bc596b14
June Young Kang <jkang34@gmail.com>
2020-10-08 22:07:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,49 +1,30 @@
-
-
-def roll_call_dwarves(name)
-  dwarves = ["Dopey", "Grumpy", "Bashful"]
+def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
     puts "#{index + 1}. #{name}"
   end
 end
 
-
-
 def summon_captain_planet(element)
-  veggies = ["carrot", "cucumber", "pepper"]
-  fruits = ["apple", "banana", "orange"]
-  if element === veggies
-    veggies.collect{|element| element.capitalize + "!"}
-  else element === fruits
-    fruits.collect{|element| element.capitalize + "!"}
-  end
+    element.collect{|element| element.capitalize + "!"}
 end
 
 def long_planeteer_calls(call)
-  long_planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  short_planeteer_calls = ["wind", "fire"] 
-  if call === long_planeteer_calls
-    long_planeteer_calls.any? do |word|
-      word.length > 4
-    end
-  else call === short_planeteer_calls
-    short_planeteer_calls.all? do |word|
-      word.length < 4
-    end
+  call.any? do |word|
+    word.length > 4
   end
 end
 
 def find_the_cheese(cheese)
-  cheddar_cheese = ["banana", "cheddar", "sock"]
   cheese_types = ["cheddar", "gouda", "camembert"]
-  no_cheese = ["ham", "cellphone", "computer"]
-  if cheese === cheddar_cheese
-    cheddar_cheese.find do |word|
+    cheese.find do |word|
       cheese_types.include?(word)
     end
-  else cheese === no_cheese
-    if no_cheese.include?(word = "cheddar" || word = "gouda" || "camembert")
-      return nil 
-    end
-  end
 end
+
+
+def words_with_b(words)
+  words.select do |word|
+    word[0] == "b"
+  end
+end  
+
